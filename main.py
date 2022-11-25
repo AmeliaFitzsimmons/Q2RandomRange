90ae0f17504d97e9f93fa2b95da435839fc45625
Amelia Fitzsimmons <113189695+AmeliaFitzsimmons@users.noreply.github.com>
2022-11-25 15:05:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a = float(input('Enter a number (enter lowest number first):'))
+b = float(input('Enter another number (enter higher number second):'))
+# Program to generate a random number between a and b
+
+# importing the random module
+import random
+
+print(random.randint(a,b))
